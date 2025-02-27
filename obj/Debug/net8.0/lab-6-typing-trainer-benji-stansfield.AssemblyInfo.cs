21d5a2a7850fb228bdfef3d907e8ce31abd5110b
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-02-27 12:13:55 -0700

    Set cursor position to (0,0) and let's the user type over text

--- obj/Debug/net8.0/lab-6-typing-trainer-benji-stansfield.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-6-typing-trainer-benji-stansfield.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-6-typing-trainer-benji-stansfield")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b0601bfff6bd32a44fa361569ef70d7b45dd6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f516ba7b6d8b472393b21e931a9d1b5cac2445d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-6-typing-trainer-benji-stansfield")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-6-typing-trainer-benji-stansfield")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
