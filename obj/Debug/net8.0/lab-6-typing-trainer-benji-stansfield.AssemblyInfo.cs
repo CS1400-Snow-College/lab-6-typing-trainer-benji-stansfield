f4ff5cc90c7b0f09c2db916387bc69d7034f6367
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-03-04 13:07:32 -0700

    Missed a period

--- obj/Debug/net8.0/lab-6-typing-trainer-benji-stansfield.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-6-typing-trainer-benji-stansfield.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-6-typing-trainer-benji-stansfield")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f94e6fde0d80249c52ca28e6def77a09456883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9c3b0ed12d608f7dea531f1092dbe33b7e0491d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-6-typing-trainer-benji-stansfield")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-6-typing-trainer-benji-stansfield")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
